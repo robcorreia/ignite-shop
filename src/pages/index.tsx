60b96bcb292df6113999503c344e58d3b7812370
Robson Correia <robsoncorreia@Robsons-MBP.lan>
2022-10-15 16:29:13 -0300

    feat: add stitches in project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import { styled } from "./styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+});
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+      <Button>Button</Button>
+    </>
+  );
 }
---
+++ src/pages/styles/index.ts
@@ -0,0 +1,17 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  createTheme,
+  css,
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: "#8257e6",
+    },
+  },
+});
