9b53e39a3f46d5df4da97cab75bc40cb8fed8ed9
Robson Correia <robsoncorreia@Robsons-MBP.lan>
2022-10-15 16:48:32 -0300

    feat: add globalstyles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
 import { styled } from "./styles";
 
-const Button = styled("button", {
-  backgroundColor: "$rocketseat",
-});
-
 export default function Home() {
   return (
     <>
       <h1>Hello</h1>
-      <Button>Button</Button>
     </>
   );
 }
