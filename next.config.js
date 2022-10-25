1590aaac22d4032b68677a0e09d3a3f49d009451
Robson <robccorreia@gmail.com>
2022-10-25 12:20:13 -0300

    feat: list products from stripe

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
+  images: {
+    domains: ["files.stripe.com"],
+  },
   experimental: {
     images: {
       allowFutureImage: true,
     },
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
