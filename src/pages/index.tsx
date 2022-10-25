280480f8f1c8aa380ba5d49f83e7c04fa5a898ce
Robson <robccorreia@gmail.com>
2022-10-25 20:44:31 -0300

    feat: add staticprops SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,9 @@ import { useKeenSlider } from "keen-slider/react";
 
 import { HomeContainer, Product } from "./styles/pages/home";
 
-import camiseta1 from "../assets/camisetas/1.png";
-import camiseta2 from "../assets/camisetas/2.png";
-import camiseta3 from "../assets/camisetas/3.png";
-
 import "keen-slider/keen-slider.min.css";
 import { stripe } from "../lib/stripe";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Stripe from "stripe";
 
 interface HomeProps {
@@ -47,7 +43,8 @@ export default function Home({ products }: HomeProps) {
 }
 
 //usar somente quando a informação é obrigatóriamente necessária estar carregada na página
-export const getServerSideProps: GetServerSideProps = async () => {
+// export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
@@ -67,5 +64,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2,
   };
 };
