7266a9e10842eb6be48fcdf1a104675e632b06bc
Robson <robccorreia@gmail.com>
2022-10-26 20:04:29 -0300

    feat: add Product Component

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,27 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <>
-      <h1>Product: {JSON.stringify(query)}</h1>
-    </>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1> Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, at
+          obcaecati. In sunt sequi veniam debitis libero. Libero, ipsam, laborum
+          voluptate tenetur cupiditate fuga eveniet iste doloremque
+          exercitationem adipisci hic!
+        </p>
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
